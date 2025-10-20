e2cbc309932c9eafcd55d10812f7af9d6eeea2ad
Peter-Gergs <petergergs15@gmail.com>
2025-10-20 08:58:49 +0300

    fix search

--- product/utils.py
+++ product/utils.py
@@ -9,23 +9,17 @@ def search_products(query):
         Product.objects.annotate(
             priority=Case(
                 When(
-                    Q(name__icontains=query) | Q(name_ar__icontains=query),
+                    Q(name__icontains=query),
                     then=Value(1),
                 ),
                 When(
-                    Q(description_en__icontains=query)
-                    | Q(description_ar__icontains=query),
+                    Q(description__icontains=query),
                     then=Value(2),
                 ),
                 default=Value(3),
                 output_field=IntegerField(),
             )
         )
-        .filter(
-            Q(name__icontains=query)
-            | Q(name_ar__icontains=query)
-            | Q(description_en__icontains=query)
-            | Q(description_ar__icontains=query)
-        )
+        .filter(Q(name__icontains=query) | Q(description__icontains=query))
         .order_by("priority")
     )
