00294ad1cb15b96ecebae260e466629f41706d03
Peter-Gergs <petergergs15@gmail.com>
2025-11-09 23:52:37 +0200

    fix db

--- order/models.py
+++ order/models.py
@@ -64,6 +64,7 @@ class OrderItem(models.Model):
         null=True,
         related_name="order_items",
     )
+
     def __str__(self):
         return self.name
 
@@ -78,15 +79,15 @@ class ShippingSetting(models.Model):
 
 class PendingOrder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    customer_phone = models.CharField(max_length=20, blank=True)
-    governorate = models.CharField(max_length=100, blank=True)
-    city = models.CharField(max_length=100, blank=True)
+    customer_phone = models.CharField(max_length=255, blank=True)
+    governorate = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=255, blank=True)
     street = models.CharField(max_length=255, blank=True)
-    building_number = models.CharField(max_length=10, blank=True)
-    floor_number = models.CharField(max_length=10, blank=True)
-    apartment_number = models.CharField(max_length=10, blank=True)
+    building_number = models.CharField(max_length=255, blank=True)
+    floor_number = models.CharField(max_length=255, blank=True)
+    apartment_number = models.CharField(max_length=255, blank=True)
     landmark = models.CharField(max_length=255, blank=True)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total_amount = models.DecimalField(max_digits=255, decimal_places=2)
     is_paid = models.BooleanField(default=False)
     paymob_order_id = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
